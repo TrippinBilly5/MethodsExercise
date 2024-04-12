ec27abe56e7efcb7cd844f57e4536f810e8c945b
mark aldridge <mark.aldridge1@protonmail.com>
2024-04-12 13:49:43 -0700

    initial check-in

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,10 +1,44 @@
-﻿namespace MethodsExercise
+﻿using System.Runtime.CompilerServices;
+
+Console.WriteLine("What is your name?");
+string userName = Console.ReadLine();
+
+Console.WriteLine("How old are you?");
+string userAge = Console.ReadLine();
+
+Console.WriteLine("What is a name for a dog?");
+string userDogName = Console.ReadLine();
+
+Console.WriteLine("What color is your dog?");
+string userDogColor = Console.ReadLine();
+
+Console.WriteLine($"There was a person named {userName} who was {userAge} years old.");
+Console.WriteLine($"{userName} had a {userDogColor} dog named {userDogName}.");
+
+Console.WriteLine("Give me a number");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Give me a number to add to the first number");
+int num2 = int.Parse(Console.ReadLine());
+
+int sum = Sum(num1, num2);
+Console.WriteLine($"The sum is {sum}");
+
+Console.WriteLine("Give me a number");
+num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Give me a number to add to the first number");
+num2 = int.Parse(Console.ReadLine());
+
+int product = Multiply(num1, num2);
+Console.WriteLine($"The product is {product}");
+
+static int Sum(int num1, int num2)
 {
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+    return num1 + num2;
+}
+
+static int Multiply(int num1, int num2)
+{
+    return num1 * num2;
 }
